b1dc626a358c57669cff2ca60e34925bde91f76f
ocopulus <jlemus003@gmail.com>
2018-05-07 05:40:47 -0600

    paguina web cv

---
+++ app2/Controllers/comentariosController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using app2;
+
+namespace app2.Controllers
+{
+    public class comentariosController : Controller
+    {
+        private SemDBEntities db = new SemDBEntities();
+
+        // GET: comentarios
+        public ActionResult Index()
+        {
+            return View(db.comentario.ToList());
+        }
+
+        // GET: comentarios/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            comentario comentario = db.comentario.Find(id);
+            if (comentario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comentario);
+        }
+
+        // GET: comentarios/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: comentarios/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,nombre,recomendacion")] comentario comentario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.comentario.Add(comentario);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(comentario);
+        }
+
+        // GET: comentarios/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            comentario comentario = db.comentario.Find(id);
+            if (comentario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comentario);
+        }
+
+        // POST: comentarios/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,nombre,recomendacion")] comentario comentario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(comentario).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(comentario);
+        }
+
+        // GET: comentarios/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            comentario comentario = db.comentario.Find(id);
+            if (comentario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comentario);
+        }
+
+        // POST: comentarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            comentario comentario = db.comentario.Find(id);
+            db.comentario.Remove(comentario);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
